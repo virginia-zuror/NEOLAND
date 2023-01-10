5da741525eeb6af9598de759f1bc2985014505ac
virginia-zuror <virginiazuor@gmail.com>
2023-01-10 20:05:02 +0100

    iteracion 4 js

---
+++ JS_EXERCISES/iteracion4.js
@@ -0,0 +1,37 @@
+//ITERACION 4.1
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+//ITERACION 4.2
+
+//const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers[0]= "IRONMAN";
+
+console.log(avengers);
+
+//ITERACION 4.3
+
+const avengersSecond = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengersSecond.length);
+
+//ITERACION 4.4
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters.pop());
+
+//ITERACION 4.5
+
+const rickAndMortyCharactersSecond = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharactersSecond.pop;
+console.log(rickAndMortyCharactersSecond.shift() + " y " + rickAndMortyCharactersSecond.pop());
+
+//ITERACION 4.6
+
+const rickAndMortyCharactersThird = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharactersThird.splice(1, 1);
+console.log(rickAndMortyCharactersThird);
+
+
