324bcf82b7aedb3b12aa3aefdf833b25fa7b31a7
virginia-zuror <virginiazuor@gmail.com>
2023-01-11 23:13:56 +0100

    iteracion 2 func

--- JS_EXERCISES/neoland-basic-function/iteracion2.js
+++ JS_EXERCISES/neoland-basic-function/iteracion2.js
@@ -1,8 +1,26 @@
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+let cadenaLarga;
+
+for(let i = 0; i< avengers.length; i++){
+  let primero = avengers[0].length;
+
+  if(avengers[i].length >= primero){
+    cadenaLarga = avengers[i];
+    primero = avengers[i];
+    
+  }
+  
+}
+console.log(cadenaLarga);
 
 
 
-  for (let i=0; i< avengers.length; i++){
+
+
+
+
+
+  /*for (let i=0; i< avengers.length; i++){
     
     let cadena = avengers[0].length;
    
@@ -11,7 +29,7 @@ const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain
       console.log(cadena);
       }
     }
- 
+ */
     
 
 
