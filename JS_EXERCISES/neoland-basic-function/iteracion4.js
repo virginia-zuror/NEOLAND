d23ed8d40df4fceeb65c8fe9e47f0e36f9c0d022
virginia-zuror <virginiazuor@gmail.com>
2023-01-11 22:22:01 +0100

    iteracion 4 func

---
+++ JS_EXERCISES/neoland-basic-function/iteracion4.js
@@ -0,0 +1,11 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+//function average(param) {
+    let sum=0;  
+for (const number of numbers) {
+     sum += number;
+    }
+    
+    let average = sum/(numbers.length);
+
+    console.log(average);
+//}
