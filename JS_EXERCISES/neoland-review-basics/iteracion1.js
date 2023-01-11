bc372588bbe49e15ace7745a678d63c225ce0fb4
virginia-zuror <virginiazuor@gmail.com>
2023-01-11 19:04:53 +0100

    iteracion 3 rev basic

---
+++ JS_EXERCISES/neoland-review-basics/iteracion1.js
@@ -0,0 +1,24 @@
+
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+];
+
+
+const categoriasPeliculas=[];
+
+for (const movie of movies) {
+    for(const category of movie.categories){
+  
+         if(!categoriasPeliculas.includes(category)){
+                categoriasPeliculas.push(category);
+                categoriasPeliculas.sort()
+         }                
+    }
+}
+console.log(categoriasPeliculas);
+
+
