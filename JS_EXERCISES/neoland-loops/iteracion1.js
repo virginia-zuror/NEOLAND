d07e74cfbd170c900d6a834600317d76fef2a063
virginia-zuror <virginiazuor@gmail.com>
2023-01-11 13:03:51 +0100

    fix toUpperCase

--- JS_EXERCISES/neoland-loops/iteracion1.js
+++ JS_EXERCISES/neoland-loops/iteracion1.js
@@ -1,7 +1,7 @@
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
 let articulos =[];
 for(const product of products){
-   if(product.includes('Camiseta')){
+   if(product.toUpperCase().includes('CAMISETA')){
     articulos.push(product);
    };
 
