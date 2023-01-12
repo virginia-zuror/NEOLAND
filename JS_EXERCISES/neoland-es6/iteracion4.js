d7b912381a6767022a2b15b94c50be75b7e0c102
virginia-zuror <virginiazuor@gmail.com>
2023-01-12 21:47:59 +0100

    iteracion 4 ES6

---
+++ JS_EXERCISES/neoland-es6/iteracion4.js
@@ -0,0 +1,36 @@
+//4.1 Dado el siguiente array, devuelve un array con sus nombres 
+//utilizando .map().
+ const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const names = users.map(user => user.name);
+console.log(names); 
+
+//4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+//de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+//empiece por 'A'.
+const usersOther = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+const names2 = usersOther.map(user => user.name.includes("A")? user.name = "Anacleto": user.name);
+console.log(names2);
+
+/* 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+cuando el valor de la propiedad isVisited = true. */
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+
+const citiesList = cities.map(city => city.isVisited? `${city.name} visitado`: city.name );
+console.log(citiesList);
