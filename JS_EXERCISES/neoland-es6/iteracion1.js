ff761efacb6136715e0a203c4e2517f7b5bc0d49
virginia-zuror <virginiazuor@gmail.com>
2023-01-12 19:26:00 +0100

    iteracion 1 ES6

---
+++ JS_EXERCISES/neoland-es6/iteracion1.js
@@ -0,0 +1,11 @@
+const funcionEjercicio = (a=10,b=5) => {
+const result = a+b;
+console.log(result);
+return result};
+//1.1
+funcionEjercicio();
+//1.2
+funcionEjercicio(3,);
+//1.3
+funcionEjercicio(2,8);
+
