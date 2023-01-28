ec9cc4bb0edf90c875fdce86df6edee2d290ec94
virginia-zuror <virginiazuor@gmail.com>
2023-01-28 20:13:45 +0100

    iteracion 4b Jedi Master

---
+++ JS_EXERCISES/neoland-extra/3_JEDI_MASTER/exercise4b/app.js
@@ -0,0 +1,47 @@
+const places = [
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1570610155223-66279ba81b41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1570888233388-35d777042d9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1490440101319-4c8eb3880432?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1460453429228-912eec8be349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+  ];
+  
+  const div = document.createElement("div");
+  document.body.insertAdjacentElement("afterbegin", div);
+  for (const place of places) {
+      div.innerHTML += `<li>
+      <h4>${place.title}</h4>
+      <img src= ${place.imgUrl} alt = ${place.title}/
+      </li>`
+  };
+
+const button = document.querySelector("button");
+
+const myDiv = document.querySelector("div");
+ 
+
+button.addEventListener("click", () => {
+        myDiv.removeChild(myDiv.lastElementChild);
+           
+    })
+      
