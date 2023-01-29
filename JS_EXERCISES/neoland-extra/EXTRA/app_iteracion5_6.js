cb398a7614255bc9f021841402b999e4a45e81e1
virginia-zuror <virginiazuor@gmail.com>
2023-01-29 22:08:02 +0100

    iteracion 5.6

---
+++ JS_EXERCISES/neoland-extra/EXTRA/app_iteracion5_6.js
@@ -0,0 +1,27 @@
+/* 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'. */
+const streamers4 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+
+const result = (param) =>{ 
+   
+    const names = streamers4.filter(streamer => streamer.name.toUpperCase().includes(param.toUpperCase()));
+    return names;
+};
+
+
+
+ 
+let input = document.querySelector("input");
+
+input.addEventListener("focusout", () => {
+    console.log(result(input.value));
+});
+
