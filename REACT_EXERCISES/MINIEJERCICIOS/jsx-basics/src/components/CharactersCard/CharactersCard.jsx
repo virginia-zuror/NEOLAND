cd568486c1c56ae1a5b650797d8d65e9fc0ceea1
virginia-zuror <virginiazuor@gmail.com>
2023-02-08 18:28:06 +0100

    jsx-basics

---
+++ REACT_EXERCISES/MINIEJERCICIOS/jsx-basics/src/components/CharactersCard/CharactersCard.jsx
@@ -0,0 +1,12 @@
+const CharactersCard = ({character})=> {
+
+    return (
+    <figure>
+      <h1>{character.name}</h1>
+      <p>{character.race}</p>
+      <p>{character.location}</p>
+    </figure>
+    );
+};
+
+export default CharactersCard;
